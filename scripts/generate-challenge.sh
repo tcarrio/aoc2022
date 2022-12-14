1a667701875770a121f58b0e2a0ef4775d61a628
Tom Carrio <tom@carrio.dev>
2022-12-14 00:19:01 -0500

    build: update template generator to support Linux sed

--- scripts/generate-challenge.sh
+++ scripts/generate-challenge.sh
@@ -10,6 +10,32 @@ assert_truthy "$PROJECT_ROOT"
 
 TEMPLATE_DIR="$PROJECT_ROOT/templates/challenge"
 
+mac_replace_templates() {
+    local f="$1"
+    local day_number="$2"
+
+    sed -i '.old' "s/##DAY##/${day_number}/g" "$f"
+    mv "$f" "${f%.template}"
+    rm "$f.old"
+}
+
+linux_replace_templates() {
+    local f="$1"
+    local day_number="$2"
+
+    sed -i "s/##DAY##/${day_number}/g" "$f"
+    mv "$f" "${f%.template}"
+}
+
+replace_templates() {
+    case "$(uname)" in
+        Darwin)
+            mac_replace_templates "$@";;
+        *)
+            linux_replace_templates "$@";;
+    esac
+}
+
 main() {
     local day_number="$1"
 
@@ -24,10 +50,8 @@ main() {
 
     cp -r "$TEMPLATE_DIR" "$target_dir"
 
-    find "$target_dir" -type f -name '*.tpl' | while read f; do
-        sed -i '.old' "s/##DAY##/${day_number}/g" "$f"
-        mv "$f" "${f%.tpl}"
-        rm "$f.old"
+    find "$target_dir" -type f -name '*.template' | while read f; do
+        replace_templates "$f" "$day_number"
     done
 }
 
